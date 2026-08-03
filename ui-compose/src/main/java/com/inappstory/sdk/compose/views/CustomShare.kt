922063e987dc9a815663c95395491b0c8388cef3
paperrose <paperrose@mail.ru>
2026-08-03 09:57:11 +0300

    update CustomShare

--- ui-compose/src/main/java/com/inappstory/sdk/compose/views/CustomShare.kt
+++ ui-compose/src/main/java/com/inappstory/sdk/compose/views/CustomShare.kt
@@ -1,53 +1,14 @@
 package com.inappstory.sdk.compose.views
 
 import android.app.Activity
-import android.content.ClipData
-import android.content.ClipboardManager
 import android.content.Context
-import android.content.Context.CLIPBOARD_SERVICE
 import android.content.pm.PackageManager
 import android.os.Build
 import android.view.View
 import android.widget.FrameLayout
-import android.widget.Toast
-import androidx.compose.animation.AnimatedVisibility
-import androidx.compose.animation.core.tween
-import androidx.compose.animation.fadeIn
-import androidx.compose.animation.fadeOut
-import androidx.compose.animation.slideInVertically
-import androidx.compose.animation.slideOutVertically
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.wrapContentHeight
-import androidx.compose.foundation.layout.wrapContentSize
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.collectAsState
-import androidx.compose.runtime.getValue
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.platform.ComposeView
-import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.semantics.contentDescription
-import androidx.compose.ui.semantics.semantics
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import com.inappstory.sdk.InAppStoryManager
-import com.inappstory.sdk.network.JsonParser
 import com.inappstory.sdk.share.IASShareData
 import com.inappstory.sdk.share.IASShareManager
 import com.inappstory.sdk.stories.callbacks.OverlappingContainerActions
@@ -56,9 +17,6 @@ import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.SupervisorJob
 import kotlinx.coroutines.delay
-import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.asStateFlow
-import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
 import java.util.HashMap
 
@@ -66,7 +24,8 @@ class CustomShare(
     private val shareContent: @Composable (
         shareData: HashMap<String, Any>,
         actions: OverlappingContainerActions
-    ) -> Unit
+    ) -> Unit,
+    private val updateViewVisibility: (Boolean)->Unit,
 ) : ShareCallback {
     private var actions: OverlappingContainerActions? = null
 
@@ -74,9 +33,6 @@ class CustomShare(
         instance = this
         InAppStoryManager.getInstance()?.setShareCallback(this)
     }
-
-    private var _viewIsVisible = MutableStateFlow(false);
-    private val viewIsVisible = _viewIsVisible.asStateFlow()
 
     private var coreScope = CoroutineScope(Dispatchers.IO + SupervisorJob())
 
@@ -151,7 +107,7 @@ class CustomShare(
     override fun viewIsVisible(view: View?) {
         coreScope.launch {
             delay(200)
-            _viewIsVisible.update { true }
+            updateViewVisibility(true)
         }
     }
 
@@ -161,7 +117,7 @@ class CustomShare(
 
     fun hideView(success: Boolean) {
         coreScope.launch {
-            _viewIsVisible.update { false }
+            updateViewVisibility(false)
             delay(500)
             if (success) {
                 shareSuccess()
